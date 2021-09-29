ffaef922d6fa32a6ace3d1aa885134d9fc7a26d6
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-09-29 09:12:25 -0400

    Update challengeone.js

---
+++ challengone.js
@@ -0,0 +1,22 @@
+console.log('for');
+
+const students = [
+    'Tre',
+    'Sonny',
+    'Crystal',
+    'Ilyas',
+    'Greg',
+    'Fernando',
+    'Timothy',
+    'Patrick',
+    'Steve',
+    'Jimothy',
+    'Pat',
+    'Arnold',
+    'Andy'
+  ];
+
+for (let i = 0; i < array1.length; i++) {
+    const students = array1[i];
+    console.log('i:', i, ', students:', students);
+}
