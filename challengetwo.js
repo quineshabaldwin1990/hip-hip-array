f766b1b77cd344193ccb46c215b2f093532a9033
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-09-29 10:00:36 -0400

    Update challengethree.js

--- challengetwo.js
+++ challengetwo.js
@@ -3,6 +3,6 @@ console.log('for');
 const grades = [100, 80, 110, 75, 83, 64];
 
 for (let i = grades.length - 1; i >= 0; i = i - 1){
-    const value = grade[i];
+    const value = grades[i];
     console.log('i:', i,  ', value:', value);
 }
